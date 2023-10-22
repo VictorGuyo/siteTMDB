22934d0e259d41621f0b07b4fd787cd51e51af44
VICTOR GUYOMARD <victorguyo44600@gmail.com>
2023-10-22 20:56:58 +0200

    Router done

--- movieSite/src/App.jsx
+++ movieSite/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import Home from "./pages/Home"
+import LikePage from "./pages/Likepage"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello REACT</h1>
-    </div>
+   <BrowserRouter>
+   <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/coups-de-coeur" element={<LikePage />} />
+    <Route path="*" element={<Home />} />
+   </Routes>
+   </BrowserRouter>
   );
 };
 
---
+++ movieSite/src/pages/Likepage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Likepage = () => {
+  return (
+    <div>
+      <h1>LIKEPAGEEEE</h1>
+    </div>
+  );
+};
+
+export default Likepage;
